099d6391a78bd2ee117cbc65f05581e553a8b457
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-07 17:38:46 +0100

    Injected AppSettings into SpotifyClientService

--- SpotifyPlaylistTool/SpotifyApi/SpotifyClientService.cs
+++ SpotifyPlaylistTool/SpotifyApi/SpotifyClientService.cs
@@ -1,19 +1,23 @@
 using JSpotifyClient;
 using JSpotifyClient.Types;
+using SpotifyPlaylistTool.Settings;
 
 namespace SpotifyPlaylistTool.SpotifyApi;
 
 public sealed class SpotifyClientService
 {
     private readonly ISpotifyClient _spotifyClient;
+    private readonly AppSettings _appSettings;
 
-    public SpotifyClientService(ISpotifyClient spotifyClient)
+    public SpotifyClientService(ISpotifyClient spotifyClient, AppSettings appSettings)
     {
         _spotifyClient = spotifyClient;
+        _appSettings = appSettings;
     }
 
-    public async Task<List<PlaylistItem>> GetPlaylistsForUserId(string userId)
+    public async Task<List<PlaylistItem>> GetPlaylistsForStoredUserId()
     {
+        var userId = _appSettings.SpotifyUserId;
         var response = await _spotifyClient.GetPlaylistsForUserId(userId);
 
         if (response.IsFailure)
