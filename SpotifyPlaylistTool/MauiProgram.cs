88726778c729038a29f24bd7f94897546e116372
jakeahumphreys <jakehumphreys@outlook.com>
2023-04-07 15:24:53 +0100

    Added a really basic settings mechanism for storage of client id and client secret

--- SpotifyPlaylistTool/MauiProgram.cs
+++ SpotifyPlaylistTool/MauiProgram.cs
@@ -1,5 +1,5 @@
-﻿using JSpotifyClient;
-using SpotifyPlaylistTool.SpotifyApi;
+﻿using System.Text.Json;
+using SpotifyPlaylistTool.Settings;
 
 namespace SpotifyPlaylistTool;
 
@@ -17,7 +17,32 @@ public static class MauiProgram
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
+
+        var appSettings = LoadOrCreateSettings();
+
+        builder.Services.AddSingleton(appSettings);
         
         return builder.Build();
+    }
+
+    private static AppSettings LoadOrCreateSettings()
+    {
+        var appDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Spotify Playlist Tool");
+
+        if (!Directory.Exists(appDirectory))
+            Directory.CreateDirectory(appDirectory);
+
+        var settingsFilePath = Path.Combine(appDirectory, "appsettings.json");
+        if (!File.Exists(settingsFilePath))
+        {
+            var appSettings = new AppSettings();
+            var appSettingsJsonString = JsonSerializer.Serialize(appSettings);
+            File.WriteAllText(settingsFilePath, appSettingsJsonString);
+            return appSettings;
+        }
+
+        var settingsFileJson = File.ReadAllText(settingsFilePath);
+
+        return JsonSerializer.Deserialize<AppSettings>(settingsFileJson);
     }
 }
---
+++ SpotifyPlaylistTool/Settings/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace SpotifyPlaylistTool.Settings;
+
+public sealed class AppSettings
+{
+    public string SpotifyClientId { get; set; }
+    public string SpotifyClientSecret { get; set; }
+}
